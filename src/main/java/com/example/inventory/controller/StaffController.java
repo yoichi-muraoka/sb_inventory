35e771898eb0ad063dd690772832682a9bc03b56
zdis56 <zdis56@sim.zdrv.com>
2021-09-13 16:41:34 +0900

    備品リストの表示

--- src/main/java/com/example/inventory/controller/StaffController.java
+++ src/main/java/com/example/inventory/controller/StaffController.java
@@ -1,14 +1,22 @@
 package com.example.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.inventory.service.ItemService;
 
 @Controller
 public class StaffController {
 
+	@Autowired
+	private ItemService itemService;
+
 	// 備品リスト
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("itemList", itemService.getAll());
 		return "index";
 	}
 
