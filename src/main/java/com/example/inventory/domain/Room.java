3db5159a7656535e92df1156de12e128ffeacf7e
zdis56 <zdis56@sim.zdrv.com>
2021-09-14 11:40:09 +0900

    PlacementMapperの作成と実装

---
+++ src/main/java/com/example/inventory/domain/Room.java
@@ -0,0 +1,11 @@
+package com.example.inventory.domain;
+
+import lombok.Data;
+
+@Data
+public class Room {
+
+	private String id;
+	private String name;
+
+}
---
+++ src/main/java/com/example/inventory/mapper/PlacementMapper.java
@@ -0,0 +1,14 @@
+package com.example.inventory.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.inventory.domain.Placement;
+
+@Mapper
+public interface PlacementMapper {
+
+	List<Placement> selectByItemId(int itemId);
+
+}
