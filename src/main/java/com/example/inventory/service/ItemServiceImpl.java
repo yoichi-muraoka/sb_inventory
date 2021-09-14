14b8346707109461ff44c45597801f1e3e0dfc49
zdis56 <zdis56@sim.zdrv.com>
2021-09-14 14:32:02 +0900

    総数の表示

--- src/main/java/com/example/inventory/service/ItemServiceImpl.java
+++ src/main/java/com/example/inventory/service/ItemServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.inventory.domain.Item;
+import com.example.inventory.domain.Placement;
 import com.example.inventory.mapper.ItemMapper;
 import com.example.inventory.mapper.PlacementMapper;
 
@@ -28,8 +29,19 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public Item getOneById(int id) {
 		Item item = itemMapper.selectById(id);
-		// itemに配置情報をまとめる
+
+		// 配置情報
+		List<Placement> list = placementMapper.selectByItemId(id);
+
+		// 備品の総数
+		int amount = 0;
+		for(Placement p : list) {
+			amount += p.getAmount();
+		}
+
+		// itemに配置情報、備品総数をまとめる
 		item.setPlacementList(placementMapper.selectByItemId(id));
+		item.setAmount(amount);
 		return item;
 	}
 
