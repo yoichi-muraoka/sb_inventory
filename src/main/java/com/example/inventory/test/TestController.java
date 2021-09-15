4111db49edcf78c32f60fb4cbcbcc545d66dbcbf
zdis56 <zdis56@sim.zdrv.com>
2021-09-15 17:29:49 +0900

    ItemServiceテスト

--- src/main/java/com/example/inventory/test/TestController.java
+++ src/main/java/com/example/inventory/test/TestController.java
@@ -5,34 +5,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.inventory.domain.Item;
-import com.example.inventory.mapper.ItemMapper;
-import com.example.inventory.mapper.PlacementMapper;
+import com.example.inventory.service.ItemService;
 
 @RestController
 public class TestController {
 
 	@Autowired
-	private ItemMapper itMapper;
+	private ItemService itService;
 
-	@Autowired
-	private PlacementMapper plMapper;
 
 	@GetMapping("/test")
 	public String test() {
-		for(Item item : itMapper.selectLimited(2, 3)) {
+		System.out.println("\nページ辺りの表示件数----");
+		itService.setNumPerPage(3);
+		System.out.println(itService.getNumPerPage());
+
+		System.out.println("\n全ページ数----");
+		System.out.println(itService.getTotalPages());
+		System.out.println(itService.getTotalPagesByRoomId("R201"));
+
+		System.out.println("\n3ページ目のデータ----");
+		for(Item item : itService.getByPage(3)) {
 			System.out.println(item);
 		}
 
-		System.out.println("\n---------\n");
-
-		for(Item item : itMapper.selectLimitedByRoomId("R211", 1, 3)) {
+		System.out.println("\n3ページ目のデータ(倉庫)----");
+		for(Item item : itService.getByRoomIdAndPage("R101", 2)) {
 			System.out.println(item);
 		}
-
-		System.out.println("\n---------\n");
-
-		System.out.println(plMapper.countDistinctByItemId());
-		System.out.println(plMapper.countByRoomId("R211"));
 
 		return "test";
 	}
