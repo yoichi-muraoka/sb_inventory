2cb6ac80bb96846245099d9e25c924524617e0d3
zdis56 <zdis56@sim.zdrv.com>
2021-09-17 12:23:10 +0900

    備品追加フォームの表示

--- src/main/java/com/example/inventory/controller/AdminController.java
+++ src/main/java/com/example/inventory/controller/AdminController.java
@@ -1,12 +1,21 @@
 package com.example.inventory.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.inventory.domain.Item;
 import com.example.inventory.service.ItemService;
 
 @Controller
@@ -15,6 +24,13 @@ public class AdminController {
 
 	@Autowired
 	private ItemService itemService;
+
+	@InitBinder
+	public void initBinderForm(WebDataBinder binder) {
+		var dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class,  new CustomDateEditor(dateFormat, true));
+	}
+
 
 	@GetMapping
 	public String index(
@@ -33,6 +49,21 @@ public class AdminController {
 			itemService.deleteById(id);
 		}
 		return "redirect:/admin";
+	}
+
+	@GetMapping("/add")
+	public String add(Model model) {
+		Item item = new Item();
+		item.setPurchasedAt(new Date()); // 購入日の初期値を本日に設定
+		model.addAttribute("item", item);
+		return "admin/add";
+	}
+
+	@PostMapping("/add")
+	public String add(
+			@ModelAttribute Item item,
+			Model model) {
+		return "admin/add";
 	}
 
 }
