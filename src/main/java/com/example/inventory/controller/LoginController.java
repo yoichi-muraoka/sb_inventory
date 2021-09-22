f23376580fffc554e70c62daed965549f7ff2f6b
zdis56 <zdis56@sim.zdrv.com>
2021-09-22 13:23:03 +0900

    LoginControllerの作成とバリデーションの実装

---
+++ src/main/java/com/example/inventory/controller/LoginController.java
@@ -0,0 +1,53 @@
+package com.example.inventory.controller;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.inventory.domain.Admin;
+import com.example.inventory.service.AdminService;
+
+@Controller
+@RequestMapping("/admin")
+public class LoginController {
+
+	@Autowired
+	AdminService adminService;
+
+	@Autowired
+	HttpSession session;
+
+	@GetMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("admin", new Admin());
+		return "admin/login";
+	}
+
+	@PostMapping("/login")
+	public String login(
+			@Valid Admin admin,
+			Errors errors,
+			Model model) {
+		if(errors.hasErrors()) {
+			// 未入力
+			return "admin/login";
+		}
+		else if(!adminService.login(admin.getLoginId(), admin.getLoginPass(), session)) {
+			// ログインID、またはパスワードが間違っている
+			errors.rejectValue("loginId", "wrong_id_or_password");
+			return "admin/login";
+		}
+
+		// ログイン成功
+		return "redirect:/admin";
+	}
+
+
+}
